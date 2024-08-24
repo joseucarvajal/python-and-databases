44fad60658e67ef5902aaa1072b9f42bc0b516ec
joseucarvajal <joseucarvajal@gmail.com>
2024-08-23 19:09:36 -0500

    Saving course to database

--- python_college_manager/app.py
+++ python_college_manager/app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
 import streamlit as st
+
+load_dotenv()
 
 st.title("College grades app")
--- python_college_manager/pages/create_course_page.py
+++ python_college_manager/pages/create_course_page.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
 import streamlit as st
+import mysql.connector
+from mysql.connector import Error
 
-load_dotenv()
 
 st.title("Create course")
 name = st.text_input('Course name')
@@ -16,3 +16,37 @@ submit = st.button("Submit")
 if submit:
     st.write(f"Course name is: {name}")
     st.write(f"database name: {os.getenv("DB_NAME")}")
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)
+
+            query = """
+            INSERT INTO courses (name, start_date, end_date, cut1_percentage, cut2_percentage, cut3_percentage)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+
+            values = (
+                name,
+                start_date,
+                end_date,
+                cut1_percentage,
+                cut2_percentage,
+                cut3_percentage
+            )
+
+            cursor.execute(query, values)
+            connection.commit()
+
+            cursor.close()
+
+            st.write("The course has been successfully saved")
+
+    except Error as e:
+        st.error("Error connecting to database")
+        connection = None
